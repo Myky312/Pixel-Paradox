0048f5778aea7410e38dca43861dd46eff8a1894
Myktybek <sattarovmyktybek255@gmail.com>
2023-11-25 21:00:33 +0100

    "latest commit"

--- frontend/src/Components/LoginSignup/Signup.jsx
+++ frontend/src/Components/LoginSignup/Signup.jsx
@@ -33,8 +33,19 @@ const Signup = () => {
       console.log(response.data);
       // Handle successful signup here (e.g., redirect to login page)
     } catch (error) {
-      console.error("Error during signup:", error);
-      // Handle errors here (e.g., show error message)
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error("Error data:", error.response.data);
+        console.error("Error status:", error.response.status);
+        console.error("Error headers:", error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Error request:", error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error', error.message);
+      }
     }
   };
 
